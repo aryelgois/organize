2e943ee9b1bbb37cdf3c9fb9f16c900a5cd562c5
Aryel Mota Góis <aryel.gois@gmail.com>
2019-05-27 16:50:12 -0300

    Add EmptystateComponent

---
+++ src/app/components/emptystate/emptystate.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emptystate',
+  templateUrl: './emptystate.component.html',
+  styleUrls: ['./emptystate.component.scss'],
+})
+export class EmptystateComponent {}
