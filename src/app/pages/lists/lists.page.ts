bfbdb0402122a2e377be09fceeafe837b0824bf2
Aryel Mota Góis <aryel.gois@gmail.com>
2019-05-27 10:30:20 -0300

    Show list of List in ListsPage

--- src/app/pages/lists/lists.page.ts
+++ src/app/pages/lists/lists.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { List } from '../../models/list';
+
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.page.html',
   styleUrls: ['./lists.page.scss'],
 })
 export class ListsPage implements OnInit {
+
+  lists: List[] = [];
 
   constructor(
   ) {}
