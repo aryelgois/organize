00ceb0500e6e23b4e5076f39d860214a36de1473
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-01 16:32:15 -0300

    Show search results in ProductsPage

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../../models/product';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
+
+  results: Product[];
 
   constructor(
   ) {}
