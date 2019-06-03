24e95933e4f04ad9d2aa8cfe60c33de3295c552d
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-03 11:14:24 -0300

    Use Keyword in AbstractProduct

--- src/app/models/abstract-product.ts
+++ src/app/models/abstract-product.ts
@@ -1,6 +1,8 @@
+import { Keyword } from './keyword';
+
 export interface AbstractProduct {
   id: string;
   name: string;
   defaultUnit: string|null;
-  keywords: string[];
+  keywords: Keyword[];
 }
