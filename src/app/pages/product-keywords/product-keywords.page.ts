2e43569f4a00f21c4fa052eed2c0d1f5b6848c70
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-03 09:37:15 -0300

    List keywords in ProductKeywordsPage

--- src/app/pages/product-keywords/product-keywords.page.ts
+++ src/app/pages/product-keywords/product-keywords.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Keyword } from '../../models/keyword';
+
 @Component({
   selector: 'app-product-keywords',
   templateUrl: './product-keywords.page.html',
@@ -7,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductKeywordsPage implements OnInit {
 
+  keywords: Keyword[];
+
   constructor(
   ) {}
 
   ngOnInit() {
+  }
+
+  voteDown(keyword: Keyword) {
+    keyword.score -= 1;
+  }
+
+  voteUp(keyword: Keyword) {
+    keyword.score += 1;
   }
 
 }
