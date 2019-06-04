a92f1cc37db5278362b5c4fed4cfd91277a6d87e
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-04 13:51:34 -0300

    Use qrcode module to generate QR Codes in SVG

--- src/app/pages/list-qr-code/list-qr-code.page.ts
+++ src/app/pages/list-qr-code/list-qr-code.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import QRCode from 'qrcode';
+
 @Component({
   selector: 'app-list-qr-code',
   templateUrl: './list-qr-code.page.html',
@@ -8,11 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class ListQrCodePage implements OnInit {
 
   code: string;
+  qrcode: string;
 
   constructor(
   ) {}
 
   ngOnInit() {
+    if (this.code) {
+      const url = 'http://example.com/lists?qrcode=' + this.code;
+
+      QRCode.toString(url, { margin: 0 })
+        .then((svg: string) => this.qrcode = 'data:image/svg+xml;base64,' + btoa(svg))
+        .catch((err) => console.error(err));
+    }
   }
 
 }
