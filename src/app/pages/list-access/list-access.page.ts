aa54d927fc92ade5720b9bb7c259e5869d5491ca
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-06 16:46:12 -0300

    List users that can access a list

--- src/app/pages/list-access/list-access.page.ts
+++ src/app/pages/list-access/list-access.page.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/user';
+
 @Component({
   selector: 'app-list-access',
   templateUrl: './list-access.page.html',
   styleUrls: ['./list-access.page.scss'],
 })
 export class ListAccessPage implements OnInit {
+
+  owner: User;
+  users: User[];
+  anonymous: number;
 
   constructor(
   ) {}
