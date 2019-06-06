dcee9072e5a2adba3ab51d6895ee2338ed5fe094
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-06 19:04:25 -0300

    Add owner to List model

--- src/app/models/list.ts
+++ src/app/models/list.ts
@@ -1,7 +1,9 @@
 import { Product } from './product';
+import { User } from './user';
 
 export interface List {
   id: string;
+  owner: User;
   name: string;
   image: string|null;
   products: Product[];
