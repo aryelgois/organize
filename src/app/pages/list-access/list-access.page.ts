01e3fbaea487599391287a881d16eaec535d7680
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-06 18:48:43 -0300

    Present options in ListAccessPage

--- src/app/pages/list-access/list-access.page.ts
+++ src/app/pages/list-access/list-access.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActionSheetController } from '@ionic/angular';
+
 import { User } from '../../models/user';
 
 @Component({
@@ -13,10 +15,54 @@ export class ListAccessPage implements OnInit {
   users: User[] = [];
   anonymous = 0;
 
+  isAnonymousAllowed: boolean;
+  isLinkEnabled: boolean;
+
   constructor(
+    private actionSheetCtrl: ActionSheetController,
   ) {}
 
   ngOnInit() {
+  }
+
+  async presentAccessOptions() {
+    const buttons = [
+      {
+        text: 'Alterar dono',
+        icon: 'share-alt',
+        role: 'destructive',
+        handler: () => console.log('Alterar dono'),
+      },
+      this.isAnonymousAllowed
+        ? {
+            text: 'Bloquear anônimos',
+            icon: 'anonymous-off',
+            handler: () => console.log('Bloquear anônimos'),
+          }
+        : {
+            text: 'Permitir anônimos',
+            icon: 'anonymous',
+            handler: () => console.log('Permitir anônimos'),
+          },
+      this.isLinkEnabled
+        ? {
+            text: 'Desativar compartilhamento por link',
+            icon: 'link-off',
+            handler: () => console.log('Desativar compartilhamento por link'),
+          }
+        : {
+            text: 'Ativar compartilhamento por link',
+            icon: 'link',
+            handler: () => console.log('Ativar compartilhamento por link'),
+          },
+    ];
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Opções de compartilhamento',
+      buttons,
+    });
+
+    return await actionSheet.present();
   }
 
 }
