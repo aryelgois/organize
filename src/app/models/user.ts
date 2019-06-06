b9696cc80d21bcbbd7543e599fdc9edf3e5724f9
Aryel Mota Góis <aryel.gois@gmail.com>
2019-06-06 19:26:19 -0300

    Add function to check if two users are the same

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,3 +4,9 @@ export interface User {
   id: string;
   person: Person|null;
 }
+
+export function isSameUser(userA: User, userB: User): boolean {
+  const personA = userA.person;
+  const personB = userB.person;
+  return userA.id === userB.id || (personA && personB && personA.id === personB.id);
+}
